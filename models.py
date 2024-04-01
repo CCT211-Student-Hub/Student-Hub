1ab0aa2e7098c3303c40b6bf1bfe04af4cf6fc18
William Ma <65773478+willmadev@users.noreply.github.com>
2024-04-01 15:15:52 -0400

    Database classes

--- models.py
+++ models.py
@@ -1,63 +1,157 @@
 from __future__ import annotations
+import sqlite3
+from typing import Optional
+
+db_path = "student_hub.db"
+
+class Sqlite_Db:
+    """A class that handles connections to the database"""
+    cur: sqlite3.Cursor
+
+    def __init__(self) -> None:
+        # connect to db
+        self.con = sqlite3.connect(db_path)
+        self.con.execute("PRAGMA foreign_keys = 1")
+        cur = self.con.cursor()
+
+        # initialize db
+        cur.execute("""
+                         CREATE TABLE IF NOT EXISTS course (
+                            course_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+                            course_name TEXT NOT NULL
+                         );
+                         """)
+        cur.execute("""
+                        CREATE TABLE IF NOT EXISTS task (
+                            task_id     INTEGER PRIMARY KEY AUTOINCREMENT,
+                            title       TEXT NOT NULL,
+                            description TEXT NOT NULL,
+                            completed   INTEGER NOT NULL DEFAULT 0,
+                            course_id   INTEGER NOT NULL,
+                            FOREIGN KEY (course_id) REFERENCES course (course_id) ON DELETE CASCADE
+                        );
+                        """)
+
 
 class Task:
     """A model that represents a task"""
-    def __init__(self) -> None:
-        self.task_id = 1
+    task_id: int
+    title: str
+    description: str
+    completed: bool
+    course_id: int
 
-    def get_all_tasks() -> list[Task]:
-        """Retrieves a user's tasks"""
-        # TODO
-        pass
+    def __init__(self, task_id: int, title: str, description: str, completed: bool, course_id: int) -> None:
+        self.task_id = task_id
+        self.title = title
+        self.description = description
+        self.completed = completed
+        self.course_id = course_id
 
-
-    def get_tasks_by_course(course_code: str) -> list[Task]:
-        """Retrieves a user's tasks from a given course code"""
-        # TODO
-        pass
-
-
-    def create_task(course_code: str) -> Task:
-        """Create a task and enter into database"""
-        # TODO
-        pass
-
-
-    def update_task(task_id: str) -> Task:
+    def update(self, db: Sqlite_Db, title: str=None, description:str=None, completed:bool=None, course_id:int=None):
         """Update a task that already exists"""
-        # TODO
-        pass
+        if title is None: title = self.title
+        if description is None: description = self.description
+        if completed is None: completed = self.completed
+        if course_id is None: course_id = self.course_id
+        cur = db.con.cursor()
+        cur.execute("UPDATE task SET title=?, description=?, completed=?, course_id=? WHERE task_id=?;", (title, description, int(completed), course_id, self.task_id))
+        db.con.commit()
+        self.title = title
+        self.description = description
+        self.completed = completed
+        self.course_id = course_id
 
-
-    def delete_task(task_id: str) -> bool:
+    def delete(self, db: Sqlite_Db):
         """Delete a task from the database"""
-        # TODO
-        pass
+        cur = db.con.cursor()
+        cur.execute("DELETE FROM task WHERE task_id=?;", (self.task_id,))
+        db.con.commit()
 
+    def get_task(db: Sqlite_Db, task_id: int) -> Optional[Task]:
+        """Retrieves a task by task_id. Returns the task"""
+        cur = db.con.cursor()
+        res = cur.execute("SELECT task_id, title, description, completed, course_id FROM task WHERE task_id=?;", (task_id, ))
+        result = res.fetchone()
+        if result is None:
+            return None
+        task = Task(result[0], result[1], result[2], bool(result[3]), result[4])
+        return task
+        
+    def get_all_tasks(db: Sqlite_Db) -> list[Task]:
+        """Retrieves a user's tasks. Returns a list of Task objects"""
+        cur = db.con.cursor()
+        res = cur.execute("SELECT task_id, title, description, completed, course_id FROM task;")
+        results = res.fetchall()
+        tasks = []
+        for task in results:
+            tasks.append(Task(task[0], task[1], task[2], bool(task[3]), task[4]))
+        return tasks
+
+    def get_tasks_by_course(db: Sqlite_Db, course_id: int) -> list[Task]:
+        """Retrieves a user's tasks from a given course id"""
+        cur = db.con.cursor()
+        res = cur.execute("SELECT task_id, title, description, completed, course_id FROM task WHERE course_id = ?;", (course_id, ))
+        results = res.fetchall()
+        tasks = []
+        for task in results:
+            tasks.append(Task(task[0], task[1], task[2], bool(task[3]), task[4]))
+        return tasks
+
+    def create_task(db: Sqlite_Db, title, description, completed, course_id) -> Task:
+        """Create a task and enter into database. Returns the task_id of the task"""
+        cur = db.con.cursor()
+        cur.execute("INSERT INTO task (title, description, completed, course_id) VALUES (?, ?, ?, ?);", (title, description, int(completed), course_id))
+        db.con.commit()
+        return Task.get_task(db, cur.lastrowid)
 
 
 class Course:
     """A model that represents a course"""
+    course_id: int
+    course_name: str
 
-    def get_all_courses() -> list[Course]:
-        """Retrieves a user's courses"""
-        # TODO
-        pass
+    def __init__(self, course_id: int, course_name: str) -> None:
+        self.course_id = course_id
+        self.course_name = course_name
 
-
-    def create_course(course_name: str) -> Course:
-        """Create a course and enter into database"""
-        # TODO
-        pass
-
-
-    def update_course(course_code: str) -> Course:
+    def update(self, db: Sqlite_Db, course_name: str=None):
         """Update a course that already exists"""
-        # TODO
-        pass
+        if course_name is None: course_name = self.course_name
+        cur = db.con.cursor()
+        cur.execute("UPDATE course SET course_name=? WHERE course_id=?;", (course_name, self.course_id))
+        db.con.commit()
+        self.course_name = course_name
 
-
-    def delete_course(course_code: str) -> bool:
+    def delete(self, db: Sqlite_Db) -> bool:
         """Delete a course from the database"""
-        # TODO
-        pass
+        cur = db.con.cursor()
+        cur.execute("DELETE FROM course WHERE course_id=?;", (self.course_id,))
+        db.con.commit()
+
+    def get_all_courses(db: Sqlite_Db) -> list[Course]:
+        """Retrieves a user's courses"""
+        cur = db.con.cursor()
+        res = cur.execute("SELECT course_id, course_name FROM course;")
+        results = res.fetchall()
+        courses = []
+        for course in results:
+            courses.append(Course(course[0], course[1]))
+        return courses
+
+    def get_course(db: Sqlite_Db, course_id: int) -> Optional[Course]:
+        """Retrieves a single course by course_id"""
+        cur = db.con.cursor()
+        res = cur.execute("SELECT course_id, course_name FROM course WHERE course_id = ?;", (course_id, ))
+        result = res.fetchone()
+        if result is None:
+            return None
+        course = Course(result[0], result[1])
+        return course
+
+    def create_course(db: Sqlite_Db, course_name: str) -> Course:
+        """Create a course and enter into database. Returns the course_id of the course"""
+        cur = db.con.cursor()
+        cur.execute("INSERT INTO course (course_name) VALUES (?);", (course_name,))
+        db.con.commit()
+        return Course.get_course(db, cur.lastrowid)
--- populate_db.py
+++ populate_db.py
@@ -1,10 +1,24 @@
-"""A script that populates the sqlite database with sample tasks and courses"""
+"""A script that populates the sqlite database with sample tasks and course
+NOTE: This script deletes the existing database. Use with caution
+"""
 
-from models import Task
+import os
+from models import Course, Sqlite_Db, Task
 
 def populate_db():
-    # TODO
-    pass
+    # Remove existing database
+    os.remove("student_hub.db")
+
+    db = Sqlite_Db()
+
+    course_1 = Course.create_course(db, "CCT211")
+    Task.create_task(db, "Project 1", "This assignment focuses on the design, implementation and testing of a Python program to allow the user to play an arcade-style action or platformer game. This game will leverage the strengths of the Pygame library. ", False, course_1.course_id)
+    Task.create_task(db, "Project 2", "Project 2 is a creative exercise where students are expected to take what they have learned about Python and create a simple GUI application with a persistence layer.The core of the project should be a Tkinter GUI form that provides basic CRUD persistent functionality in the context of your choice. ", False, course_1.course_id)
+
+    course_2 = Course.create_course(db, "CCT212")
+    Task.create_task(db, "Term Paper pt. 1", "This is part 1 of your final essay assignment for this course. For this assignment, you need to provide an abstract and annotated bibliography of your proposed final term paper. The purpose of this assignment is to get you to begin putting together a paper based on one of the four units of the course: The History of Hacking, Hacking In Popular Culture, Free and Open Source Software, and Hacking and Capitalism. In this assignment you’ll learn how to put together a proper summary of a proposed paper, locate relevant sources, and how to discuss those sources in a critical way. The essay you will be writing will be argumentative and critical. It is not just a summary or description of events, but offers an argument with an actual thesis statement. ", False, course_2.course_id)
+    Task.create_task(db, "Term Paper pt. 2", "In part 2 of your final term paper assignment you will be writing a critical essay based on the abstract and annotated bibliography that you submitted for term paper part 1. This assignment should be in proper essay format, with an introduction, a clear thesis statement, argumentative paragraphs, and a proper conclusion. Your essay should be based on the abstract and sources you submitted for part 1, but you can make changes based on the feedback you received from part 1. You are also allowed to add additional sources as long as they are reputable, reliable, and relevant to your topic. Do not completely change your topic - instead make any necessary changes or edits based on how you did on part 1 in order to have a strong final essay. ", False, course_2.course_id)
+
 
 if __name__ == "__main__":
     populate_db()
